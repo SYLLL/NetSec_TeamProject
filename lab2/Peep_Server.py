edea77199eb32c8d8f87500bc26e09b8eda8bbd7
nding5 <nding5@jhu.edu>
2017-10-02 11:30:35 -0700

    added passthrough, but facing some errors...

--- lab2/Peep_Server.py
+++ lab2/Peep_Server.py
@@ -1,27 +1,71 @@
 import asyncio
 from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import UINT32, STRING
-from peep_packets import PEEP_Packet
+from Peep_Packets import PEEP_Packet
 from random import randint
 
-class ServerProtocol(asyncio.Protocol):
+from playground.network.common import StackingTransport,StackingProtocol,StackingProtocolFactory
+
+class PEEP_Server(asyncio.protocols):
+    def __init__(self):
+        self.transport= None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self._deserializer = PacketType.Deserializer()
+
+    def data_received(self, data):
+
+        self._deserializer.update(data)
+
+    def connection_lost(self, exc=None):
+        self.transport = None
+        print("connection has lost!")
+
+
+class PEEP_Server_Passthrough2(StackingProtocol):
+    def __init__(self):
+     #   super.__init__()
+        self.transport=None
+
+    def connection_made(self, transport):
+        print("Client: start a connection")
+        self.transport = transport
+        htransport = StackingTransport(self.transport)
+        self.higherProtocol().connection_made(htransport)
+
+    def data_received(self, data):
+        print("Client :data received")
+        self.higherProtocol().data_received(data)
+
+    def connection_lost(self, exc=None):
+        print("Client : connection lost")
+        self.higherProtocol().connection_lost()
+        self.transport = None
+
+
+class PEEP_Server_Passthrough1(StackingProtocol):
     def __init__(self):
         self.received=0
         self.transport=None
-        self._Deserializer = PacketType.Deserializer()
+        #self._Deserializer = PacketType.Deserializer()
         self.state=0
+       # super.__init__()
 
     def connection_made(self,transport):
         self.transport=transport
         self._Deserializer=PacketType.Deserializer()
         peername = transport.get_extra_info('peername')
         print('server(prepare)-->client(prepare):Connection from {}'.format(peername))
+        self.higherProtocol().connection_made(StackingTransport(transport))
 
     def data_received(self,data):
         self._Deserializer=PacketType.Deserializer()
         self._Deserializer.update(data)
         for pkt in self._Deserializer.nextPackets():
             self.handle_packets(pkt)
+        self.higherProtocol().data_received(data)
+
 
     def handle_packets(self,pkt):
         if isinstance(pkt,PEEP_Packet):
@@ -67,6 +111,7 @@ class ServerProtocol(asyncio.Protocol):
         if pkt.Checksum==0:
             print('server side: checksum of ACK is correct')
             #send data
+            #self.higherProtocol().data_received(data)
             self.state+=1
 
         else:
@@ -102,7 +147,7 @@ class ServerProtocol(asyncio.Protocol):
             pktback.Checksum=0
             self.transport.write(pktback.__serialize__())
             self.transport.close()
-            print('server side: sent RST')
+            prnt('server side: sent RST')
 
     def handle_ripack(self,pkt):
         if pkt.Checksum==0:
@@ -112,23 +157,3 @@ class ServerProtocol(asyncio.Protocol):
             print('server side: checksum of RIPACK is incorrect')
             self.transport.close()
 
-#loop = asyncio.get_event_loop()
-## Each client connection will create a new protocol instance
-#coro = loop.create_server(ServerProtocol, '127.0.0.1', 9997)
-#server = loop.run_until_complete(coro)
-#
-## Serve requests until Ctrl+C is pressed
-#print('Serving on {}'.format(server.sockets[0].getsockname()))
-#try:
-#    loop.run_forever()
-#except KeyboardInterrupt:
-#    pass
-#
-#
-## Close the server
-#server.close()
-#loop.run_until_complete(server.wait_closed())
-#loop.close()
-#
-#
-#
